4251e0e9f5adef29aa15da52f57e652b4e101d9b
Hikaru <hikaru.purba@gmail.com>
2020-02-20 23:58:50 -0700

    fixing modified files

--- Number_of_Steps_to_Reduce_a_Number_to_Zero/src/Main.java
+++ Number_of_Steps_to_Reduce_a_Number_to_Zero/src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Solution sol = new Solution();
+        int inputNum = 14;
+        System.out.println(Integer.toString(sol.numberOfSteps(inputNum)));
+    }
 }
--- Number_of_Steps_to_Reduce_a_Number_to_Zero/src/Solution.java
+++ Number_of_Steps_to_Reduce_a_Number_to_Zero/src/Solution.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Solution {
+
+    public int numSteps = 0;
+
+    public int numberOfSteps (int num) {
+        while(num != 0) {
+            // Even Number
+            if(num % 2 == 0){
+                num = num / 2;
+                numSteps++;
+            }
+            // Odd Number
+            else{
+                num = (num - 1);
+                numSteps++;
+            }
+        }
+        return numSteps;
+    }
 }
